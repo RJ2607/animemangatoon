3a563ed0e59b197e9e12e208718d2d5ea57e710b
RJ2607 <rishijain2607@gmail.com>
2024-10-11 19:39:30 +0530

    Initialize Hive with application document directory and register model adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:animemangatoon/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'Model/animeWebToonsModel.dart';
+import 'Model/characterModel.dart';
+import 'Model/detailsModel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDir.path);
+  Hive.registerAdapter(AnimeWebToonsModelAdapter());
+  Hive.registerAdapter(DetailsModelAdapter());
+  Hive.registerAdapter(CharacterModelAdapter());
+
+  await Hive.openBox<AnimeWebToonsModel>('favorites');
 
   runApp(MyApp());
 }
